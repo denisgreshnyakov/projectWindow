3a90ed6c698ae1c58e15d8ee70539201e87256ec
Denis <denisgreshnaykov@gmail.com>
2022-08-07 22:53:41 +0500

    add timer and click images

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,14 @@ import modals from "./modules/modals";
 import tabs from "./modules/tabs";
 import forms from "./modules/forms";
 import changeModalState from "./modules/changeModalState";
+import timer from "./modules/timer";
+import images from "./modules/images";
 
 window.addEventListener("DOMContentLoaded", () => {
   "use strict";
 
   let modalState = {};
+  let deadline = "2022-09-01";
 
   changeModalState(modalState);
 
@@ -27,4 +30,6 @@ window.addEventListener("DOMContentLoaded", () => {
     "inline-block"
   );
   forms(modalState);
+  timer(".container1", deadline);
+  images();
 });
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -9,6 +9,7 @@ const modals = () => {
     const modal = document.querySelector(modalSelector);
     const close = document.querySelector(closeSelector);
     const windows = document.querySelectorAll("[data-modal]");
+    const scroll = calcScroll();
 
     //my const
     const inputWidth = document.querySelector("#width");
@@ -37,6 +38,7 @@ const modals = () => {
 
           modal.style.display = "block";
           document.body.style.overflow = "hidden";
+          document.body.style.marginRight = `${scroll}px`;
         }
 
         if (item.classList.contains("popup_calc_button")) {
@@ -83,6 +85,7 @@ const modals = () => {
 
       modal.style.display = "none";
       document.body.style.overflow = "";
+      document.body.style.marginRight = `0px`;
       //document.body.classList.remove("modal-open");
     });
 
@@ -94,6 +97,7 @@ const modals = () => {
 
         modal.style.display = "none";
         document.body.style.overflow = "";
+        document.body.style.marginRight = `0px`;
         //document.body.classList.remove("modal-open");
       }
     });
@@ -106,6 +110,20 @@ const modals = () => {
     }, time);
   }
 
+  function calcScroll() {
+    let div = document.createElement("div");
+
+    div.style.width = "50px";
+    div.style.height = "50px";
+    div.style.overflowY = "scroll";
+    div.style.visibility = "hidden";
+
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+
+    return scrollWidth;
+  }
   bindModal(
     ".popup_engineer_btn",
     ".popup_engineer",
@@ -125,7 +143,7 @@ const modals = () => {
     ".popup_calc_end_close",
     false
   );
-  showModalByTime(".popup", 60000);
+  showModalByTime(".popup-window", 60000);
 };
 
 export default modals;
